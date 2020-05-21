f74e42dc54224efccbc9d7ff5fcea865e5a6471f
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-05-20 23:07:12 -0300

    Desafio ReactJS

---
+++ README.md
@@ -0,0 +1,18 @@
+# Rocketseat - Desafio: Conceitos do ReactJS
+
+## Sobre o desafio
+
+Continuação do desenvolvimento da aplicação que irá armazenar repositórios do portfólio.
+Desafio - Atingir os objetivos de cada funcionalidade:
+
+- **Listar os repositórios da sua API:** Deve ser capaz de criar uma lista com o campo title de todos os repositórios que estão cadastrados na sua API.
+- **Adicionar um repositório a sua API:** Deve ser capaz de adicionar um novo item na sua API através de um botão com o texto Adicionar e, após a criação, deve ser capaz de exibir o nome dele após o cadastro.
+- **Remover um repositório da sua API:** Para cada item da sua lista, deve possuir um botão com o texto Remover que, ao clicar, irá chamar uma função para remover esse item da lista do seu frontend e da sua API.
+
+## Start
+
+```js
+    yarn install    // instala dependências
+    yarn start      // inicia aplicação
+    yarn test       // executa os testes
+```
--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import Api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await Api.get('/repositories');
+      await setRepositories(res.data);
+    }
+    fetchData();
+  },[])
+
   async function handleAddRepository() {
-    // TODO
+    const res = await Api.post(`/repositories`, {
+      title: `Desafio ReactJS ${new Date().toLocaleString()}`,
+      url: 'https://github.com/ValchanOficial/DesafioConceitosReactJS', 
+      techs: ['NodeJS', 'ReactJS', 'Jest']
+    });
+    setRepositories([...repositories, res.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await Api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
